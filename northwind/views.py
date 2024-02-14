1f25878908a5c37f4ff048161923332fd8e83efd
Anton Oboleninov <anton.oboleninov@saritasa.com>
2024-02-15 02:14:49 +0700

    --wip-- [skip ci]

--- northwind/views.py
+++ northwind/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
+from . import models
 
 
 def orders(request):
-    return render(request, 'orders.html')
+    orders = models.Orders.objects.all()
+    return render(request, 'orders.html', {'orders': orders})
 
 
 def categories(request):
-    return render(request, 'categories.html')
+    categories = models.Categories.objects.all()
+    import ipdb; ipdb.set_trace()
+    from io import BytesIO
+    a3 = bytes.fromhex(categories[0].picture)
+    return render(request, 'categories.html', {'categories': categories})
